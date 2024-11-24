a610a27e68cf4899b8fde5bd31068dc4e2536c60
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-24 10:35:54 -0800

    ci

--- run.sh
+++ run.sh
@@ -17,4 +17,13 @@ if ! incus image info 002-homebrew-configured &>/dev/null; then
     packer build 002-homebrew-configured.pkr.hcl
 fi
 
+# Clean up existing containers
+incus info homebrew-configured &>/dev/null && incus rm --force homebrew-configured
+incus info homebrew-setup &>/dev/null && incus rm --force homebrew-setup
+
+terraform init
+
+terraform plan -out=tfplan
+terraform apply tfplan
+
 incus exec homebrew-configured -- bash -l -c 'brew install hashicorp/tap/packer'
